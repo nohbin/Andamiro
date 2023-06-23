8c8456980b14845722ed1f46cc1626fac2dbb2b4
nohbin <nohbinmusic@gmail.com>
2023-06-23 14:17:35 +0900

    0623-1

--- src/main/java/com/andamiro/controller/action/MemberAction.java
+++ src/main/java/com/andamiro/controller/action/MemberAction.java
@@ -8,5 +8,5 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface MemberAction {
 	public void excute(HttpServletRequest request , HttpServletResponse response) throws IOException , ServletException;
-	
+	void sysout();
 }
