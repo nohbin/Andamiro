3de086aa95df723a75dfd41d2d725199c8b3b0a1
nohbin <nohbinmusic@gmail.com>
2023-06-30 09:34:16 +0900

    Add files via upload

---
+++ DB/anda4.sql
@@ -0,0 +1,23 @@
+insert into andamiromember
+values(member_seq.nextval,'admin','c30a26c19c22a2b14c996cb847f9157c9864f705b20ae3abea8b346113d5f8e0','관리자','9999','9999','','','999');
+
+insert into recipekind values(01 , '한식');
+insert into recipekind values(02 , '일식');
+insert into recipekind values(03 , '양식');
+insert into recipekind values(04 , '중식');
+insert into recipekind values(99 , '기타');
+
+insert into recipehow values(1,'볶음');
+insert into recipehow values(2,'튀김');
+insert into recipehow values(3,'삶기');
+insert into recipehow values(4,'끓이기');
+insert into recipehow values(5,'굽기');
+insert into recipehow values(6,'찜');
+insert into recipehow values(99,'기타');
+
+insert into recipemainingre values(1,'돼지고기');
+insert into recipemainingre values(2,'소고기');
+insert into recipemainingre values(3,'닭고기');
+insert into recipemainingre values(4,'육류');
+insert into recipemainingre values(5,'해물');
+insert into recipemainingre values(99,'기타');
