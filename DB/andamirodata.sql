392b6b1e3deabbb46bafb13cc64b9d7222a8e7dc
nohbin <nohbinmusic@gmail.com>
2023-06-30 09:29:20 +0900

    Add files via upload

---
+++ DB/andamirodata.sql
@@ -0,0 +1,23 @@
+insert into andamiromember
+values(member_seq.nextval,'admin','c30a26c19c22a2b14c996cb847f9157c9864f705b20ae3abea8b346113d5f8e0','°ü¸®ÀÚ','9999','9999','','','999');
+
+insert into recipekind values(01 , 'ÇÑ½Ä');
+insert into recipekind values(02 , 'ÀÏ½Ä');
+insert into recipekind values(03 , '¾ç½Ä');
+insert into recipekind values(04 , 'Áß½Ä');
+insert into recipekind values(99 , '±âÅ¸');
+
+insert into recipehow values(1,'ººÀ½');
+insert into recipehow values(2,'Æ¢±è');
+insert into recipehow values(3,'»î±â');
+insert into recipehow values(4,'²úÀÌ±â');
+insert into recipehow values(5,'±Á±â');
+insert into recipehow values(6,'Âò');
+insert into recipehow values(99,'±âÅ¸');
+
+insert into recipemainingre values(1,'µÅÁö°í±â');
+insert into recipemainingre values(2,'¼Ò°í±â');
+insert into recipemainingre values(3,'´ß°í±â');
+insert into recipemainingre values(4,'À°·ù');
+insert into recipemainingre values(5,'ÇØ¹°');
+insert into recipemainingre values(99,'±âÅ¸');
